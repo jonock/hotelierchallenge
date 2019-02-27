1abac44fe8c49d357054a5059e0a8f5f32db60b8
Jonathan <jonatha.noack@student.unisg.ch>
2019-02-27 16:18:40 +0100

    minor

--- downloads_arne/twitter_hashtag.py
+++ downloads_arne/twitter_hashtag.py
@@ -4,7 +4,7 @@ from twython import Twython, TwythonError
 # Authentifizierung mit Twitter. Twitter API credentials. Sind auf der Twitter Developer Seite zu finden, wo ihr Euren Account gemacht habt.
 twitter = Twython('eNzGxqtNGa7OKlUNgTwagQN1K', 'SHVEUvhtvb6WsezK4x8O4RHZeUchePEdjwKftWl1fNvTBhXS1J')
 # Starte die Twittersuche nach @borussia
-search_results = twitter.search(q='#bahamas', count=100000)
+search_results = twitter.search(q='#bahamas', count=1)
 # Speichere die Ergebnisse in das Dictionary all_tweets
 all_tweets = search_results['statuses']
 # Ausgabe des Dictionaries in einer Schleife
