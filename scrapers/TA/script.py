e42be5e3150594b4de8367ad749afbf1ed752002
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-03-22 15:11:24 +0100

    Added loop to script.py to ensure that prices are scraped

--- scrapers/TA/script.py
+++ scrapers/TA/script.py
@@ -5,6 +5,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 #Input-File
 localities = tripadvisor_scraper.importHashtagList('city.txt')
+
 #Daten:"%Y/%m/%d"
 checkin_date = '2019/04/02'
 checkout_date = '2019/04/04'
@@ -12,5 +13,6 @@ sort = 'recommended'
 
 for locality in localities:
     print(locality + ' kommt nun')
-    data = tripadvisor_scraper.parse(locality,checkin_date,checkout_date,sort)
+    for _ in range(3):   #beim ersten scrapen nimmts die Preise nicht immer, deshalb der Loop hier.
+        data = tripadvisor_scraper.parse(locality,checkin_date,checkout_date,sort)
     tripadvisor_scraper.writeTripAdvisor(data, locality)
