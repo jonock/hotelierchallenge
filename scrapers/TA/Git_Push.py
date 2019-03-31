74492611811b183ae435da41079e47d2e97f0b0d
PPP <pierre-pascal.proeller@student.unisg>
2019-03-31 17:22:24 +0200

    Update

---
+++ scrapers/TA/Git_Push.py
@@ -0,0 +1,4 @@
+#Git: Add and commit changes
+cd ~/desktop/hotelierchallenge/scrapers/TA/scrapes/spechotel && git commit -a -m "Neue Scrapes vom 'date'"
+#send data to Git Server
+cd ~/desktop/hotelierchallenge/scrapers/TA && git push origin master
