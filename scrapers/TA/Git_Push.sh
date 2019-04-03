41244dbe2ee1e5fa424af7e87b73acf96fc6b497
PPP <pierre-pascal.proeller@student.unisg>
2019-04-03 13:38:40 +0200

    Update Git Push

--- scrapers/TA/Git_Push.sh
+++ scrapers/TA/Git_Push.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 #Git: Add and commit changes
-cd ~/desktop/hotelierchallenge/scrapers/TA/scrapes/spechotel && git commit -a -m "Neue Scrapes vom 'date'"
+cd ~/desktop/hotelierchallenge/scrapers/TA/scrapes/spechotel && git commit -a -m "Neue Scrapes"
 #send data to Git Server
 cd ~/desktop/hotelierchallenge/scrapers/TA && git push origin master
