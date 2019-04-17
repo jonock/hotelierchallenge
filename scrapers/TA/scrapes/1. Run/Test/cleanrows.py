4873810028bcc7e6ea1a4d3dfd2546f2680eacb6
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-04-17 16:49:36 +0200

    data cleaning

--- scrapers/TA/scrapes/1. Run/Test/cleanrows.py
+++ scrapers/TA/scrapes/1. Run/Test/cleanrows.py
@@ -2,13 +2,15 @@
 
 import pandas as pd
 
-prices = pd.read_csv ('test.csv')
+prices = pd.read_csv ('tripadvisor_data_https___www.tripadvisor.ch_Hotel_Review-g60763-d1938661-Reviews-Row_NYC_Hotel-New_York_City_New_York.html_2019-04-04 13-01-08.431706.csv')
 
 #To delete columns axis = 1
-#prices.drop('column', axis=1, inplace=True)
+prices.drop('platform_name', axis=1, inplace=True)
 
-
+#delete rows (axis = 0)
 prices.drop(prices.index[1:],axis = 0, inplace=True)
 
-x = prices.head(3)
-print(x)
+
+prices.to_csv("testend.csv")
+
+print("Done")
