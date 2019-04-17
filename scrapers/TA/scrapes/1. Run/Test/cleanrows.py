71a43b754bc8a126fad358682ad01c7763fd758d
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-04-17 16:36:36 +0200

    clean data

--- scrapers/TA/scrapes/1. Run/Test/cleanrows.py
+++ scrapers/TA/scrapes/1. Run/Test/cleanrows.py
@@ -1,1 +1,14 @@
 #this script will go through the hotel_price files and delete unnecessary RainbowClubSA
+
+import pandas as pd
+
+prices = pd.read_csv ('test.csv')
+
+#To delete columns axis = 1
+#prices.drop('column', axis=1, inplace=True)
+
+
+prices.drop(prices.index[1:],axis = 0, inplace=True)
+
+x = prices.head(3)
+print(x)
