a02775f6b95c3eed6f9294f0bb073f326f7d3a1d
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-04-23 12:21:20 +0200

    Excel per Hotel - Dubai appended

--- scrapers/TA/scrapes/1. Run/appenddata.py
+++ scrapers/TA/scrapes/1. Run/appenddata.py
@@ -7,12 +7,12 @@ from glob import glob
 all_data = pd.DataFrame()
 
 #open every file in specified folder
-for entry in glob('New York/*.xlsx'):
+for entry in glob('Dubai/*.xlsx'):
     with open(entry, 'r') as f:
         df = pd.read_excel(entry)
         all_data = all_data.append(df, ignore_index=True)
         #x = all_data.describe()
         #write appended data to excel
-        all_data.to_excel('NewYork_Prices_appended.xlsx')
+        all_data.to_excel('Dubai_Prices_appended.xlsx')
 
 print('Dates appended - Excel saved')
