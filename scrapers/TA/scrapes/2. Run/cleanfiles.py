a0473f7293011534d9182875ecbe57d03281b397
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-04-27 13:54:18 +0200

    Let's go.. running script for 12k files. wish me luck

--- scrapers/TA/scrapes/2. Run/cleanfiles.py
+++ scrapers/TA/scrapes/2. Run/cleanfiles.py
@@ -5,29 +5,20 @@ import os
 import xlsxwriter
 from glob import glob
 
-for entry in glob('Hotel_Einzeln/*.csv'):
+for entry in glob('Test/*.csv'):
     with open(entry, 'r') as f:
         prices = pd.read_csv (entry)
 
 #To delete columns axis = 1
         try:
-            prices.drop('platform_name', axis=1, inplace=True)
+            #prices.drop('platform_name', axis=1, inplace=True)
 #delete rows (axis = 0)
             prices.drop(prices.index[1:],axis = 0, inplace=True)
-            prices.replace(",", ".", inplace=True)
+            #prices.replace(",", ".", inplace=True)
         except:
             next
 #writing the same csv with cleaned and encoded data
         prices.to_csv(entry, header=True, index=False, encoding='utf-8-sig')
+        print ("Data cleaned - CSV saved")
 
-print ("Data cleaned")
-
-#writing to excel to avoid csv issues in prices_data
-for entry in glob('Hotel_Einzeln/*csv'):
-    with open(entry, 'r') as f:
-        data = pd.read_csv(entry)
-        writer = pd.ExcelWriter(entry + 'output.xlsx', engine='xlsxwriter')
-        data.to_excel(writer, index=False)
-        writer.save()
-
-print("Excel saved")
+print("BATCH DONE - WOOT WOOT PARTEEEEY")
