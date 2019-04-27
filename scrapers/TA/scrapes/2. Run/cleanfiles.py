f0ce5175b5ab1c0dc48cab3ed6764b2538be635a
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-04-27 13:45:39 +0200

    Weka Preparation 2. Run Files

---
+++ scrapers/TA/scrapes/2. Run/cleanfiles.py
@@ -0,0 +1,33 @@
+#this script will go through the hotel_price files and delete unnecessary cells
+
+import pandas as pd
+import os
+import xlsxwriter
+from glob import glob
+
+for entry in glob('Hotel_Einzeln/*.csv'):
+    with open(entry, 'r') as f:
+        prices = pd.read_csv (entry)
+
+#To delete columns axis = 1
+        try:
+            prices.drop('platform_name', axis=1, inplace=True)
+#delete rows (axis = 0)
+            prices.drop(prices.index[1:],axis = 0, inplace=True)
+            prices.replace(",", ".", inplace=True)
+        except:
+            next
+#writing the same csv with cleaned and encoded data
+        prices.to_csv(entry, header=True, index=False, encoding='utf-8-sig')
+
+print ("Data cleaned")
+
+#writing to excel to avoid csv issues in prices_data
+for entry in glob('Hotel_Einzeln/*csv'):
+    with open(entry, 'r') as f:
+        data = pd.read_csv(entry)
+        writer = pd.ExcelWriter(entry + 'output.xlsx', engine='xlsxwriter')
+        data.to_excel(writer, index=False)
+        writer.save()
+
+print("Excel saved")
