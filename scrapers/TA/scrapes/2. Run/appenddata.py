f863e733bb7df1287e2f57fbfeed3fb5f4f0fae4
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-04-27 16:56:53 +0200

    appended Hotel Files for PPPPPPPPPP

--- scrapers/TA/scrapes/2. Run/appenddata.py
+++ scrapers/TA/scrapes/2. Run/appenddata.py
@@ -1,4 +1,4 @@
-#this script will go through the hotel_price files and create one file with all data per hotel
+#this script will go through the hotel_price files and create one file with all data
 
 import pandas as pd
 from glob import glob
@@ -7,12 +7,12 @@ from glob import glob
 all_data = pd.DataFrame()
 
 #open every file in specified folder
-for entry in glob('Singapore/*.xlsx'):
+for entry in glob('Luxury Bahia Jamaica/*.csv'):
     with open(entry, 'r') as f:
-        df = pd.read_excel(entry)
+        df = pd.read_csv(entry)
         all_data = all_data.append(df, ignore_index=True)
         #x = all_data.describe()
         #write appended data to excel
-        all_data.to_excel('Singapore_Prices_appended.xlsx')
+        all_data.to_csv('Luxury Bahia Jamaica_Prices_appended.csv', header=True, index=False, encoding='utf-8-sig' )
 
 print('Dates appended - Excel saved')
