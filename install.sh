fc0f272d65c92c852a291ad3ca5f12a58eb7ba5b
Jonathan <jonock@gmail.com>
2019-05-01 12:49:27 +0000

    Jetzt läuft Selenium

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ sudo apt-get install libxml2-dev -y libxslt1-dev -y
 sudo apt-get install python3-lxml python-lxml -y
 echo Erste Huerde halbwegs geschafft, jetzt kommt der Python Kram
 echo Es folgt Selenium
-wget https://chromedriver.storage.googleapis.com/75.0.3770.8/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/73.0.3683.68/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 sudo chmod a+x chromedriver
-cp ./chromedriver /scrapers/specHotelChains/
+cp ./chromedriver scrapers/specHotelChains/
 echo Ok, jetzt noch die Python extensions
 pip install -r requirements.txt
 echo ok, das wars
