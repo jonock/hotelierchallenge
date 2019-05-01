0b83f50ccb114425e74c9af6b63c483bd63eb9b9
Jonathan <jonock@gmail.com>
2019-05-01 07:43:13 +0000

    installationsskript

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo Jetzt wird lxml installiert
+sudo apt-get install unzip -y
 sudo apt-get install libxml2-dev -y libxslt1-dev -y
 sudo apt-get install python3-lxml python-lxml -y
-echo Erste Hürde halbwegs geschafft, jetzt kommt der Python Kram
+echo Erste Huerde halbwegs geschafft, jetzt kommt der Python Kram
 echo Es folgt Selenium
 wget https://chromedriver.storage.googleapis.com/75.0.3770.8/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
-./chromedriver
 sudo chmod a+x chromedriver
 cp chromedriver /scrapers/specHotelChains
 echo Ok, jetzt noch die Python extensions
