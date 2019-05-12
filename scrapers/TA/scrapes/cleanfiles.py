efcb767de39581adc74b6118b53a671a7ceca80f
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-05-12 12:54:30 +0200

    cleaned files

--- scrapers/TA/scrapes/cleanfiles.py
+++ scrapers/TA/scrapes/cleanfiles.py
@@ -5,18 +5,10 @@ import os
 import xlsxwriter
 from glob import glob
 
-for entry in glob('Test2/*.csv'):
+for entry in glob('Kontrolle/*.csv'):
     with open(entry, 'r') as f:
         prices = pd.read_csv (entry)
-
-#To delete columns axis = 1
-        try:
-            #prices.drop('platform_name', axis=1, inplace=True)
-#delete rows (axis = 0)
         prices.drop(prices.index[1:],axis = 0, inplace=True)
-            #prices.replace(",", ".", inplace=True)
-        except:
-            next
 #writing the same csv with cleaned and encoded data
         prices.to_csv(entry, header=True, index=False, encoding='utf-8-sig')
         print ("Data cleaned - CSV saved")
