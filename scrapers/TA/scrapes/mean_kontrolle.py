9f9d2fa79062d296f411d24be8a8a0a7987180e8
Jonathan Noack <jonock@gmail.com>
2019-05-12 18:58:43 +0200

    für tobi

--- scrapers/TA/scrapes/mean_kontrolle.py
+++ scrapers/TA/scrapes/mean_kontrolle.py
@@ -19,13 +19,18 @@ for sfolder in os.listdir(rootdir):
     avg = pd.DataFrame()
     all_data = pd.DataFrame()
     avgmean_appended = []
+    avgmean_timestamps = []
     avg.drop(avg.index, inplace=True)
     sfoler = childdir + '/*.csv'
     n=0
-    checklist = ['test','testtest']
+    checklist = ['checklist']
 #2. Schleife Jedes File in gegebenem Ordner durchgeben
     for entry in glob(sfoler):
         print(entry)
+        # Falls File bereits gelesen wird übersprungen
+        if entry in checklist:
+            print('CONTINUE')
+            continue
 # Jedes file wird geöffnet und als Dataframe eingelesen -> Timestamp wird extrahiert
         with open(entry, 'r') as f:
             df = pd.read_csv(entry)
@@ -33,43 +38,43 @@ for sfolder in os.listdir(rootdir):
             timestamp = df.iloc[0,1]
             #print(timestamp)
             short_timestamp = timestamp[:13]
-#           print('Short-timestamp ' +short_timestamp)
-#3. Schleife - Alle Files im Ordner werden erneut durchlaufen
+            #3. Schleife - Alle Files im Ordner werden erneut durchlaufen
             for searchfname in glob(sfoler):
-# Falls File bereits gelesen wird übersprungen
-                if searchfname in checklist:
-                    print('CONTINUE')
-                    continue
                 #print(searchfname)
+                #Überprüfung ob Zeitstempel in einem der Filenamen vorkommt
                 if short_timestamp.replace(':','-') in searchfname:
-                    print(checklist)
                     checklist.append(searchfname)
-                    #print(searchfname + 'GEFUNDEN')
                     with open(searchfname, 'r') as g:
                         dg = pd.read_csv(searchfname)
                         all_data = all_data.append(dg, ignore_index=True)
                         #print ('all_data: ' + all_data)
-                avgvar = (all_data.loc[:, "price_per_night"]).replace(' ','')
-                #avgvar.apply(pd.to_numeric) #errors='coerce'
-                avgvar = avgvar.str.strip()
-                avgvar = pd.to_numeric(avgvar)
+                    avgvar = (all_data.loc[:, "price_per_night"]).replace(' ','')
+                    #avgvar.apply(pd.to_numeric) #errors='coerce'
+                    avgvar = avgvar.str.strip()
+                    avgvar = pd.to_numeric(avgvar)
                 #print(avgvar)
 #        print(avgvar)
         avgmean = avgvar.mean()
+        #Einzelner Durschnitt gerechnet - in avgmean abgespeichert
         avgmean_appended.append(avgmean)
+        avgmean_timestamps.append(short_timestamp)
+#        print(avgmean_timestamps)
         avgmean_df = pd.DataFrame(data=avgmean_appended)
-        print(avgmean_df)
+        avgmean_timestamps_df = pd.DataFrame(data=avgmean_timestamps)
+#        print(avgmean_df)
 #        print(str(avgmean_appended)+ ' jetzt wird appended')
+        short_timestamp = 0
         all_data.drop(all_data.index, inplace=True)
         avgmean = 0
 
 #        print(str(checklist))
 
     filename = sfolder + str(short_timestamp)+'_appended.csv'
-    output = pd.DataFrame([['Mean_prices_per_night', 'Timestamp'],[avgmean_df, short_timestamp]])
-    output.to_csv('Test/appended/' + filename, header=True, index=False, encoding='utf-8-sig')
+    output = pd.DataFrame([['Mean_prices_per_night', 'Timestamp'], [avgmean_df.iloc[:1], avgmean_timestamps_df]])
+    output.to_csv('Test/appended/' + filename, header=True, index=True, encoding='utf-8-sig')
 #    avgmean_appended.drop(avgmean_appended.index, inplace=True)
     avgmean_appended=[]
-    print(avgmean_appended)
+#    print(avgmean_appended)
+    checklist= ['leer']
 
 print('Dates appended, average caluclated - Csv saved')
