55ef78367aab14ec9668f549d7baf909a97bab40
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-05-13 12:38:53 +0200

    weka preparation

---
+++ scrapers/TA/scrapes/2. Run/add_column.py
@@ -0,0 +1,17 @@
+#this script will go through the hotel lists and add a column for class
+
+import pandas as pd
+import os
+import xlsxwriter
+import numpy as np
+from glob import glob
+
+for entry in glob('WEKA_SpecHotels/*.csv'):
+    with open(entry, 'r') as f:
+        prices = pd.read_csv (entry)
+        prices['Influencer_or_Not'] = 'Influencer'
+
+        prices.to_csv(entry, header=True, index=False, encoding='utf-8-sig')
+        print (str(entry) + " New column added")
+
+print("BATCH DONE - WOOT WOOT PARTEEEEY")
