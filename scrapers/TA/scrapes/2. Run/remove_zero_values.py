4c293c46f3e4ca00a535aa3d36373e6314d3f6fc
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-05-14 11:02:24 +0200

    All Hotels with Class without Zero Values

--- scrapers/TA/scrapes/2. Run/remove_zero_values.py
+++ scrapers/TA/scrapes/2. Run/remove_zero_values.py
@@ -1,4 +1,5 @@
-#this script will go through the hotel_price files and create one file with all data
+#this script will go through the WEKA hotel files with classes and delete rows that contain
+#a zero value in the column "price_per_night" and then appends all the data into one csv.
 
 import pandas as pd
 from glob import glob
@@ -7,9 +8,8 @@ from glob import glob
 for entry in glob('WEKA Hotels Backup/*.csv'):
     with open(entry, 'r') as f:
         df = pd.read_csv(entry)
+        df['price_per_night'] = df['price_per_night'].astype(str)
         df['price_per_night'] = df['price_per_night'].str.strip()
-        #print(df['price_per_night'])
-        #df = pd.to_numeric(df['price_per_night'])
         indexNames = df[df['price_per_night'] == '0' ].index
         df.drop(indexNames, inplace=True)
         df.to_csv(entry, header=True, index=False, encoding='utf-8-sig' )
@@ -22,6 +22,6 @@ for entry in glob('WEKA Hotels Backup/*.csv'):
     with open(entry, 'r') as f:
         df = pd.read_csv(entry)
         all_data = all_data.append(df, ignore_index=True)
-        all_data.to_csv('All Hotels with Class', header=True, index=False, encoding='utf-8-sig' )
+        all_data.to_csv('All Hotels with Class_without Zero values', header=True, index=False, encoding='utf-8-sig' )
 
 print('Data appended - Excel saved')
