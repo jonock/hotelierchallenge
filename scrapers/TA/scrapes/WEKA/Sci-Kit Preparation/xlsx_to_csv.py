bf43408ece528a13d1f70250e1847adb362b549d
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-05-15 11:29:09 +0200

    new preparation file versuch 7.csv

--- scrapers/TA/scrapes/WEKA/Sci-Kit Preparation/xlsx_to_csv.py
+++ scrapers/TA/scrapes/WEKA/Sci-Kit Preparation/xlsx_to_csv.py
@@ -2,9 +2,9 @@ import pandas as pd
 from glob import glob
 
 
-for entry in glob('data/Versuch_5.xlsx'):
+for entry in glob('data/Versuch_6.xlsx'):
     with open(entry, 'r') as f:
-        df = pd.read_excel(entry, sheet_name='Versuch_5')
+        df = pd.read_excel(entry, sheet_name='Versuch_6')
 
         df.to_csv(entry + 'new.csv', header=True, index=True)
 
