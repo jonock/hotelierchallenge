a51e7b88380d129edbe26524194f9391e39f0c87
TobiasRordorf <tobias.rordorf@student.unisg.ch>
2019-05-15 15:01:22 +0200

    SciKit Jupyter Notebook update

--- scrapers/TA/scrapes/WEKA/Sci-Kit Preparation/xlsx_to_csv.py
+++ scrapers/TA/scrapes/WEKA/Sci-Kit Preparation/xlsx_to_csv.py
@@ -2,10 +2,10 @@ import pandas as pd
 from glob import glob
 
 
-for entry in glob('data/Versuch_6.xlsx'):
+for entry in glob('data/Versuch_8.xlsx'):
     with open(entry, 'r') as f:
         df = pd.read_excel(entry, sheet_name='Versuch_6')
 
-        df.to_csv(entry + 'new.csv', header=True, index=True)
+        df.to_csv(entry + 'new.csv', header=True, index=False)
 
 print('Dates appended - CSV saved')
